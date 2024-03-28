b877659a653eec3c0e9981b30fd37d0a35e74235
endlessloop2 <mateo@endlessloop.me>
2024-03-28 13:55:25 -0300

    Hello world 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'hello_world/index'
+
+  # health status (remove)
+  get "up" => "rails/health#show", as: :rails_health_check
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+
+  root "hello_world#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
