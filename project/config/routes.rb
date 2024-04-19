2d0da3ed94e967dcb6eea11e56db36e38bf27ae6
Esteban-pRo177 <esteban.ortega@uc.cl>
2024-04-19 18:20:42 -0400

    Created Home Controller

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
-  get 'hello_world/index'
+  #get 'hello_world/index'
 
+  root to: "home#index"
+  devise_scope :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
+  
   # health status (remove)
-  get "up" => "rails/health#show", as: :rails_health_check
+  #get "up" => "rails/health#show", as: :rails_health_check
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-
-  root "hello_world#index"
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
