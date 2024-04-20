d9bb7680a1ac385c80cc965e87bef3b7624f7ad2
Esteban-pRo177 <esteban.ortega@uc.cl>
2024-04-19 23:16:11 -0400

    Validates User Name

--- project/app/models/user.rb
+++ project/app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 16 }, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\z/ }
 end
