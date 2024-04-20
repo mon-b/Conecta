df32147b768afd8facf78ab3c7e941c4e200aee3
Esteban-pRo177 <esteban.ortega@uc.cl>
2024-04-20 00:37:50 -0400

    Create Categories

--- project/db/seeds.rb
+++ project/db/seeds.rb
@@ -9,3 +9,21 @@
 
 a = User.new(name: "Esteban11", email: "esteban@hacker.com", password: "uwu232", password_confirmation: "uwu232", birth_date: "2006-11-16 00:00:00.000000000 +0000")
 a.save!
+
+deportes = Category.new(name: "Deportes")
+musica = Category.new(name: "Musica")
+libros = Category.new(name: "Libros")
+cine = Category.new(name: "Cine")
+gamers = Category.new(name: "Gamers")
+otakus = Category.new(name: "Otakus")
+foodies = Category.new(name: "Foodies")
+otros = Category.new(name: "Otros")
+deportes.save!
+musica.save!
+libros.save!
+cine.save!
+gamers.save!
+otakus.save!
+foodies.save!
+otros.save!
+
