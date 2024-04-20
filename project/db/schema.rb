93d603a65538deefa8ac814a0160f02e45bfaa1e
Esteban-pRo177 <esteban.ortega@uc.cl>
2024-04-20 00:58:05 -0400

    Create Group Model

--- project/db/schema.rb
+++ project/db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_19_224111) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_20_045228) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.bigint "category_id"
+    t.bigint "user_id"
+    t.string "name"
+    t.decimal "rating"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_groups_on_category_id"
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +48,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_19_224111) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "groups", "categories"
+  add_foreign_key "groups", "users"
 end
