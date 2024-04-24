5f7ec1d9193a622401f7650c32c55f2e6f7b4b17
Esteban-pRo177 <esteban.ortega@uc.cl>
2024-04-24 18:17:23 -0400

    New Activity View

--- project/app/controllers/activity_controller.rb
+++ project/app/controllers/activity_controller.rb
@@ -7,6 +7,32 @@ class ActivityController < ApplicationController
   end
 
   def new
+    if !user_signed_in?
+      redirect_to '/users/sign_in'
+    end
+  end
+
+  def new_activity
+    @activity = Activity.new(group_id: params[:group_id],
+                    name: params[:name],
+                    location: params[:location],
+                    date: params[:date],
+                    description: params[:description],
+                    picture: params[:picture],
+                    keywords: params[:keywords],
+                    cost: params[:cost],
+                    people: params[:people])
+    if @activity.save
+      redirect_to @activity
+    else
+      flash[:errors] = @activity.errors.full_messages
+      redirect_to '/new', locals: { activity: @activity }
+    end
+  end
+
+            
+  def activity_params
+    params.require(:activity).permit(:group_id, :name, :location, :date, :description, :picture, :keywords, :cost, :people)
   end
 
   def edit
