b9c1c201d213550777a6aa54842101d184980c1d
Esteban-pRo177 <esteban.ortega@uc.cl>
2024-04-24 23:46:34 -0400

    Created Table UsersGroups and Groups Controller Mod

--- project/app/models/group.rb
+++ project/app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :category
-  belongs_to :user
+  #belongs_to :user
+  has_and_belongs_to_many :users
 
   validates :name, presence: true, length: { maximum: 32 }, uniqueness: true, format: { with: /\A[a-zA-Z0-9 ]+\z/ }
   validates :category_id, presence: true
