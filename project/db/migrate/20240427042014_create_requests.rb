a7d1a88bc94d00eb4a62aebd3d82d6916e824182
Esteban-pRo177 <esteban.ortega@uc.cl>
2024-04-27 00:39:14 -0400

    Migrate Added

--- project/db/migrate/20240427042014_create_requests.rb
+++ project/db/migrate/20240427042014_create_requests.rb
@@ -1,8 +1,8 @@
 class CreateRequests < ActiveRecord::Migration[7.0]
   def change
     create_table :requests do |t|
-      t.belongs_to :user, null: false, foreign_key: true
-      t.belongs_to :group, null: false, foreign_key: true
+      t.belongs_to :user, index: true, foreign_key: true
+      t.belongs_to :group, index: true, foreign_key: true
       t.string :status
       t.string :description
 
