be750c74cecd4dce5173d6e485dacb64f6e9f153
Mateo L <mateo@endlessloop.me>
2024-04-28 13:28:21 -0400

    Manage requests and join groups (#33)
    * Add links to view group and request to join in activity show
    Update request index
    
    * Added options to manage group requests and modified update and destroy controller
    
    * Add validation to prevent adding a user to a group if they are already a member
    
    * Add validation to prevent adding a user to a group if they are admin
    
    * Fix flash error messages in request controller and new.html.erb view

--- project/app/controllers/request_controller.rb
+++ project/app/controllers/request_controller.rb
@@ -26,8 +26,21 @@ class RequestController < ApplicationController
 
   def new_request
     exist = Request.find_by(group_id: params[:group_id], user_id: params[:user_id])
+    # WIP: añadir validacion de que el usuario no sea ya de ese grupo
+    group = Group.find(params[:group_id])
+    user = User.find(params[:user_id])
+    if group.user_id == current_user.id
+      flash[:danger] = ['Eres admin de este grupo, no te puedes unir']
+      redirect_to '/new'
+      return
+    end
+    if group.users.include?(user)
+      flash[:danger] = ['El usuario ya es miembro de este grupo']
+      redirect_to '/new'
+      return
+    end
     if exist
-      flash[:errors] = ['Ya tienes una solicitud pendiente para este grupo']
+      flash[:danger] = ['Ya tienes una solicitud pendiente para este grupo']
       redirect_to '/new'
     else
 
@@ -44,13 +57,50 @@ class RequestController < ApplicationController
     end
   end
 
+
+  def update
+  # Esta ruta permite aceptar una request
+  @request = Request.find(params[:id])
+  @group = @request.group
+  if current_user.id != @group.user_id
+    head :forbidden
+    return
+  end
+  # solo se pueden editar peticiones pendientes y no se puede cambiar el estado al mismo
+  # falta filtrar que solo puede ser pending, accepted o rejected
+  if @request.status != "pending" || @request.status == params[:status]
+    # head :bad_request
+    flash[:danger] = ['No puedes editar una request aceptada/denegada']
+    redirect_to request_index_path
+    return
+  end
+
+    @request.status = params[:status]
+    if @request.save
+      if params[:status] == "accepted"
+        @group.users << User.find(@request.user_id)
+      end
+      flash[:success] = ['Aceptaste una request correctamente']
+      redirect_to request_index_path
+      # head :ok
+    else
+      head :unprocessable_entity
+    end
+  end
+
   def edit
   end
 
   def destroy
     @request = Request.find(params[:id])
-    @request.destroy
-    redirect_to '/request'
+    if @request.status == "pending"
+      @request.destroy
+      head :ok
+    else
+      flash[:warning] = ['No puedes destruir una request aceptada/denegada']
+      redirect_to request_index_path
+      # head :bad_request
+    end
   end
 
   # Metodo para saber si desplegar o no boton de Solicitud
