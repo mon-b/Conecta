45327224f882e91488735ca19eb781c350454571
endlessloop2 <mateo@endlessloop.me>
2024-05-16 11:10:58 -0400

    chore: Add profile picture field to user registration form and fix devise filter

--- project/app/controllers/application_controller.rb
+++ project/app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :birth_date, :profile_picture])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :birth_date, :profile_picture])
   end
 end
