3b47ed5b253bc8162c5386ecdb623649cc7d67e2
endlessloop2 <mateo@endlessloop.me>
2024-05-16 17:07:44 -0400

    test: Add request destroy tests

--- project/test/controllers/request_controller_test.rb
+++ project/test/controllers/request_controller_test.rb
@@ -11,4 +11,22 @@ class RequestControllerTest < ActionDispatch::IntegrationTest
     assert_select 'div', 'request_esteban12' # fixture user_two_request_1
   end
 
+
+  test "should get request destroy if user owns the request and request is pending" do
+    sign_in users(:esteban12)
+    delete request_path(requests(:user_two_request_2))
+    assert_response :ok
+  end
+
+  test "should not destroy destroy if user owns the request and request is not pending" do
+    sign_in users(:esteban12)
+    delete request_path(requests(:user_two_request_1))
+    assert_redirected_to request_index_path
+  end
+  test "should not destroy request if request was not created by user" do
+    sign_in users(:esteban11)
+    delete request_path(requests(:user_two_request_1))
+    assert_response :forbidden
+  end
+
 end
