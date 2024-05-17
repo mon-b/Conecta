f36d4fce7f8f7797d0831188a7eac780e478e644
endlessloop2 <mateo@endlessloop.me>
2024-05-17 16:23:16 -0400

    ChatChannel subscribe and unsubscribe

--- project/app/channels/chat_channel.rb
+++ project/app/channels/chat_channel.rb
@@ -1,11 +1,11 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
-    # stream_from params[:channel]
+    stream_from params[:channel]
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
-    # stop_stream_from params[:channel]
+    stop_stream_from params[:channel]
   end
 end
