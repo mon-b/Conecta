2bf9b3d374d5f7a87ae6cc9a9fc4f1876fe26ace
endlessloop2 <mateo@endlessloop.me>
2024-05-17 15:52:25 -0400

    feat: Modify message model, add auto broadcast and add has many to user and group

--- project/app/models/message.rb
+++ project/app/models/message.rb
@@ -1,4 +1,15 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
+
+  after_create_commit { broadcast_message }
+
+  private
+
+  def broadcast_message
+    ActionCable.server.broadcast([:channel], {
+      id: id,
+      content: content
+    })
+  end
 end
