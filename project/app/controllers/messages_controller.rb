76b2696c208a1f39217268d762aa03e3f4bf1a9e
endlessloop2 <mateo@endlessloop.me>
2024-05-17 15:52:45 -0400

    feat:  create message in MessagesController

--- project/app/controllers/messages_controller.rb
+++ project/app/controllers/messages_controller.rb
@@ -1,2 +1,13 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    current_user
+    @message = current_user.messages.new(messages_params)
+
+    if @message.save
+      render json: @message, status: :created, location: @message
+    else
+      render json: @message.errors, status: :unprocessable_entity
+    end
+  end
 end
