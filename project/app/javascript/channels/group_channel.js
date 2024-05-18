f811c11fe427ee92215d3630968db94a86102321
endlessloop2 <mateo@endlessloop.me>
2024-05-18 17:49:43 -0400

    !!: Created new chat room logic and modular JS implementation

--- project/app/javascript/channels/group_channel.js
+++ project/app/javascript/channels/group_channel.js
@@ -1,15 +1,46 @@
 import consumer from "channels/consumer"
 
-consumer.subscriptions.create("GroupChannel", {
+import {renderChatRoom, renderMessage} from "../chat_room_module"
+
+// code from chat_room.js
+
+document.addEventListener('DOMContentLoaded',() => {
+
+  const currentGroupUrl = document.URL.substring(0, document.URL.lastIndexOf('/'));
+  fetch(`${currentGroupUrl}/messages_json`)
+  .then(response => response.json())
+  .then(chatRoomObject => {
+      renderChatRoom(chatRoomObject)
+  })
+})
+
+// end
+
+
+function getCurrentGroupId()
+{
+  const currentGroupUrl = document.URL.substring(0, document.URL.lastIndexOf('/'));
+  const currentGroupId = currentGroupUrl.split('/').slice(-1)[0];
+  return currentGroupId
+}
+
+consumer.subscriptions.create({
+  id: getCurrentGroupId(),
+  channel: 'GroupChannel'
+}, {
   connected() {
+    console.log('WebSocket connected correctly.');
     // Called when the subscription is ready for use on the server
   },
 
   disconnected() {
+    console.log('WebSocket closed.');
     // Called when the subscription has been terminated by the server
   },
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
+    console.log("Recieved from socket: ", data);
+    renderMessage(data);
   }
 });
