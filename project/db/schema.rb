4b4ea1b9ad55fea0bbb5c4ade6c4d7cf104dcdea
endlessloop2 <mateo@endlessloop.me>
2024-05-18 15:57:48 -0400

    Squashed commit of the following:
    commit 18a4987909dcda31fcb36384c6902133cd44bd19
    Author: Monse <monse.benavides@uc.cl>
    Date:   Sat May 18 15:22:25 2024 -0400
    
        fix button size
    
    commit d47f6b894bd8bb202014b11b49a2d37ff618169a
    Merge: 04f6284 e3cdbab
    Author: Mateo L <mateo@endlessloop.me>
    Date:   Sat May 18 14:43:28 2024 -0400
    
        Merge pull request #47 from IIC2143/whatsapp-3
    
        Whatsapp 3
    
    commit 04f628479ce9e18b39909a4f35ca6918267fc551
    Author: Monse <monse.benavides@uc.cl>
    Date:   Sat May 18 11:37:41 2024 -0400
    
        fix: cancel button height
    
    commit 0f8c1ae6bb5655e220fde26b019a20911d45ff95
    Merge: de0e268 9ea4394
    Author: Mon <125843723+mon-b@users.noreply.github.com>
    Date:   Sat May 18 11:34:22 2024 -0400
    
        Merge pull request #46 from IIC2143/Reviews
    
        Review model creation (basic)
    
    commit 9ea43943ca4058014cf107c362391b240a23673a
    Author: Monse <monse.benavides@uc.cl>
    Date:   Sat May 18 11:34:03 2024 -0400
    
        Review model creation (basic)

--- project/db/schema.rb
+++ project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_17_193709) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_17_214852) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -100,6 +100,17 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_17_193709) do
     t.index ["user_id"], name: "index_requests_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "activity_id", null: false
+    t.bigint "user_id", null: false
+    t.float "rating"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activity_id"], name: "index_reviews_on_activity_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -125,4 +136,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_17_193709) do
   add_foreign_key "messages", "users"
   add_foreign_key "requests", "groups"
   add_foreign_key "requests", "users"
+  add_foreign_key "reviews", "activities"
+  add_foreign_key "reviews", "users"
 end
