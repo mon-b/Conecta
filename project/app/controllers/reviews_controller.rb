6b2965d7f3629ec852407c07727a399e489dae86
Monse <monse.benavides@uc.cl>
2024-05-18 19:09:07 -0400

    checkpoint

--- project/app/controllers/reviews_controller.rb
+++ project/app/controllers/reviews_controller.rb
@@ -2,6 +2,25 @@ class ReviewsController < ApplicationController
     def index
     end
 
-    def new
+    def new_review
+      # create
+      activity = Activity.find(params[:activity_id])
+      user = User.find(params[:user_id])
+      review = Review.new(review_params)
+      review.activity = activity
+      review.user = user
+
+      if review.save
+        redirect_to @activity
+      else
+        flash[:errors] = review.errors.full_messages
+        redirect_back_or_to '/', locals: { review: review }
+      end
+  
+        
+    end
+
+    def review_params
+      params.require(:review).permit(:rating, :comment, :user_id, :activity_id)
     end
   end
