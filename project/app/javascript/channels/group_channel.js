fd92e24ce8371e09b210d4fbdcf43062f058398e
endlessloop2 <mateo@endlessloop.me>
2024-05-19 01:26:04 -0400

    js possible fix

--- project/app/javascript/channels/group_channel.js
+++ project/app/javascript/channels/group_channel.js
@@ -1,6 +1,33 @@
 import consumer from "channels/consumer"
 
-import {renderChatRoom, renderMessage} from "../chat_room_module"
+
+// Most of the code here comes from 
+// https://medium.com/@valentinowong/using-rails-action-cable-with-a-vanilla-javascript-front-end-1e00ed90067e
+
+const messagesListDiv = document.getElementById('messages-list');
+
+// Renders a specific chat room when a user joins it
+function renderChatRoom(chatRoomObject) {
+    chatRoomObject.forEach( messageObject => {
+        renderMessage(messageObject)
+    })
+}
+
+// Renders a specific message
+function renderMessage(messageObject) {
+    const messageDiv = document.createElement('div')
+
+    messageDiv.innerHTML = ``
+    messageDiv.textContent = messageObject.username_raw +  ": " + messageObject.content;
+    messageDiv.dataset.messageId = messageObject.id;
+
+    // messagesListDiv.prepend(messageDiv);
+    messagesListDiv.append(messageDiv); // correct order of messages. maybe because they are returned in id order ascending?
+}
+
+// test fix
+
+// import {renderChatRoom, renderMessage} from "../chat_room_module"
 
 // code from chat_room.js
 
--- project/app/javascript/chat_room_module.js
+++ project/app/javascript/chat_room_module.js
@@ -1,25 +1,25 @@
-// Most of the code here comes from 
-// https://medium.com/@valentinowong/using-rails-action-cable-with-a-vanilla-javascript-front-end-1e00ed90067e
+// // Most of the code here comes from 
+// // https://medium.com/@valentinowong/using-rails-action-cable-with-a-vanilla-javascript-front-end-1e00ed90067e
 
-const messagesListDiv = document.getElementById('messages-list');
+// const messagesListDiv = document.getElementById('messages-list');
 
-// Renders a specific chat room when a user joins it
-function renderChatRoom(chatRoomObject) {
-    chatRoomObject.forEach( messageObject => {
-        renderMessage(messageObject)
-    })
-}
+// // Renders a specific chat room when a user joins it
+// function renderChatRoom(chatRoomObject) {
+//     chatRoomObject.forEach( messageObject => {
+//         renderMessage(messageObject)
+//     })
+// }
 
-// Renders a specific message
-function renderMessage(messageObject) {
-    const messageDiv = document.createElement('div')
+// // Renders a specific message
+// function renderMessage(messageObject) {
+//     const messageDiv = document.createElement('div')
 
-    messageDiv.innerHTML = ``
-    messageDiv.textContent = messageObject.username_raw +  ": " + messageObject.content;
-    messageDiv.dataset.messageId = messageObject.id;
+//     messageDiv.innerHTML = ``
+//     messageDiv.textContent = messageObject.username_raw +  ": " + messageObject.content;
+//     messageDiv.dataset.messageId = messageObject.id;
 
-    // messagesListDiv.prepend(messageDiv);
-    messagesListDiv.append(messageDiv); // correct order of messages. maybe because they are returned in id order ascending?
-}
+//     // messagesListDiv.prepend(messageDiv);
+//     messagesListDiv.append(messageDiv); // correct order of messages. maybe because they are returned in id order ascending?
+// }
 
-export { renderChatRoom, renderMessage };
+// export { renderChatRoom, renderMessage };
