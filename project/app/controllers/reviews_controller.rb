d5ac0f2d858737e19cf49c506f45c6c63272e8bd
endlessloop2 <mateo@endlessloop.me>
2024-05-18 23:05:07 -0400

    feat: Add check for user membership before allowing review creation
    This commit adds a check in the reviews controller's create action to ensure that the user is a member of the group associated with the activity before allowing them to create a review. If the user is not a member, a forbidden response is returned. This prevents unauthorized reviews from being created.

--- project/app/controllers/reviews_controller.rb
+++ project/app/controllers/reviews_controller.rb
@@ -5,6 +5,12 @@ class ReviewsController < ApplicationController
   def create
     # Find the activity
     @activity = Activity.find(params["review"][:activity_id])
+
+    # if the user is not a member of the group associated to the activity, it is not allowed to give a review
+    if !@activity.group.users.include?(current_user)
+      head :forbidden
+      return
+    end
     @review = @activity.reviews.new(review_params)
     @review.user = current_user
     if @review.save
@@ -19,8 +25,8 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id])
   end
 
-    private
 
+  private
   def review_params
     params.require(:review).permit(:title, :rating, :body, :activity_id)
   end
