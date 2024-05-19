26e5fb662de8f48ea0339d79b5b2991639ec9700
endlessloop2 <mateo@endlessloop.me>
2024-05-18 23:42:17 -0400

    test: fixed create new activity tests

--- project/test/controllers/activity_controller_test.rb
+++ project/test/controllers/activity_controller_test.rb
@@ -16,20 +16,31 @@ class ActivityControllerTest < ActionDispatch::IntegrationTest
 
 
   test "should create new activity" do
+    sign_in users(:esteban11)
     assert_difference("Activity.count") do
-      post activity_url, params: { activity: { title: "Some title" } }
+      post '/activity',
+           params: { activity: { group_id: 1, name: "title" , location: "aa", date: DateTime.now, keywords: "aaa",
+           description: "golas dasd", cost: 100, people: 50  } }
     end
-
-    assert_redirected_to article_path(Article.last)
-    assert_equal "Article was successfully created.", flash[:notice]
+    assert_redirected_to activity_path(Activity.last)
   end
 
   test "should not create invalid activity" do
-    assert_difference("Activity.count") do
-      post activity_url, params: { activity: { location: "locationactivity" } }
+    sign_in users(:esteban11)
+    assert_no_difference("Activity.count") do
+      post '/activity', params: { activity: { location: "locationactivity" } }
     end
-
-    assert_redirected_to activity_path(Activity.last)
-    assert_equal "Activity was successfully created.", flash[:notice]
+    assert_redirected_to '/new' # TODO: fix this
   end
 end
+
+# TODO: test to not create activity if user is not member/admin
+# test "should not create invalid activity" do
+#   assert_difference("Activity.count") do
+#     post activity_url, params: { activity: { location: "locationactivity" } }
+#   end
+
+#   assert_redirected_to activity_path(Activity.last)
+#   assert_equal "Activity was successfully created.", flash[:notice]
+# end
+# end
