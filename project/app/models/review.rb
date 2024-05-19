b35257af66ab29a8b976b255d4bc11952350a3f8
endlessloop2 <mateo@endlessloop.me>
2024-05-18 22:52:27 -0400

    validate review title existence

--- project/app/models/review.rb
+++ project/app/models/review.rb
@@ -1,8 +1,9 @@
 class Review < ApplicationRecord
   validates :activity_id, presence: true
-  validates :rating, presence: true, 
+  validates :rating, presence: true,
                       numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
   validates :body, presence: true, allow_blank: false
+  validates :title, presence: true, allow_blank: false
   belongs_to :activity
   belongs_to :user
 end
